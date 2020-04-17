167105300cd538d557ae5f117795339a9a0561c6
mbaczyn <mbaczyn@gmail.com>
2020-04-17 23:16:56 +0200

    Visual part tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+    
+  const images = [
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/1.jpg?raw=true","https://github.com/mbaczyn/angular-hangman/blob/master/src/images/2.jpg?raw=true",
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/3.jpg?raw=true",
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/4.jpg?raw=true",
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/5.jpg?raw=true",
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/6.jpg?raw=true",
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/7.jpg?raw=true",
+    "https://github.com/mbaczyn/angular-hangman/blob/master/src/images/8.jpg?raw=true"
+    ];
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'V', 'X', 'Y', 'Z'];
+
+current_image : number = 3;
+
+inc_current_image(item)
+{
+  this.current_image++
+  console.log("Item: ",item);
+  }
 }
